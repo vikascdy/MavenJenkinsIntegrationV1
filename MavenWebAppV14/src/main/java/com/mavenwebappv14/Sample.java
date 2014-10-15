2880e373da44109377c72784dfb0b7a8940fc5df
vikascdy <vkschoudhary3@gmail.com>
2014-10-15 11:15:20 +0530

    sbt sonar project committed!!!

---
+++ MavenWebAppV14/src/main/java/com/mavenwebappv14/Sample.java
@@ -0,0 +1,16 @@
+package com.mavenwebappv14;
+
+public class Sample {
+
+	public Sample() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void printissues(){
+		System.out.println("name opf the method violates the naming convention");
+	}
+	public static void main(String[] args){
+		Sample s=new Sample();
+		s.printissues();
+	}
+}
