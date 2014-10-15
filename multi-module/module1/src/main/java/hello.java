ec05ca64f50d9061fefb14e623d9a678528fc969
vikascdy <vkschoudhary3@gmail.com>
2014-10-15 14:30:26 +0530

    Language changed to java

---
+++ multi-module/module1/src/main/java/hello.java
@@ -0,0 +1,19 @@
+/**
+ * Created by saurprak on 15-10-2014.
+ */
+public class hello {
+
+   public static void main(String args[])
+   {    int a=0;
+       int b=0;
+       System.out.print("hello");
+       for(int i=0 ; i<100 ; i++){
+        a=a+1;
+           System.out.println("A value::::"+a);
+        b=b+a;
+           System.out.print(" \nB value"+b);
+       }
+
+
+   }
+}
